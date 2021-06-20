e4e485d5aa009115078ba7c59b9d68881b02183a
Abhilash Gnan <abhilashgnan@gmail.com>
2021-06-20 02:13:13 +0200

    Update readme and refactor logic

--- README.md
+++ README.md
@@ -1,47 +1,34 @@
-# vscode-timer README
+# vscode-timer
+
+<p align="center">
+  <br />
+  <a title="Learn more about Timer extension" href="http://github.com/jan25/vscode-timer"><img src="https://raw.githubusercontent.com/jan25/vscode-timer/master/assets/timer.png" alt="Timer Logo" width="50%" /></a>
+</p>
 
 Timer Extension for VS Code - Set timer to keep track of deadlines or to get breaks between coding sessions.
 
 ## Features
 
-* Set timer with commonly used durations
+* Set timer with commonly used durations (in Command palatte)
 * View time remaining in status bar (usually at bottom)
 * Click on status bar to cancel or reset timer
 
 
-For example if there is an image subfolder under your extension project workspace:
+![](https://raw.githubusercontent.com/jan25/vscode-timer/master/assets/demo.gif)
 
-\!\[feature X\]\(images/feature-x.png\)
-
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
-
-## Extension Settings
-
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
 
 ## Contribution
 
 All contributions are welcome!
 
-Please file any bug reports and suggestions at [issues section](TODO) in this repo.
+Please file any bug reports and suggestions at [issues section](https://github.com/jan25/vscode-timer/issues) in this repository.
 
 ## Release Notes
 
-### WIP 0.1.0
+### 0.1.0
 
-Initial release with all feature listed above.
-
-## TODO
-
-* Write tests and setup CI
-* Make a release
-* Publish to vscode extensions store
+Initial release with all basic features.
 
 **Enjoy!**
+
+> <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
--- src/extension.ts
+++ src/extension.ts
@@ -1,82 +1,119 @@
-import { time } from 'console';
-import { stat } from 'fs';
 import * as vscode from 'vscode';
 
+const PREDEFINED_TIMER_DURATIONS = ['10m', '15m', '30m', '1h', '2h'];
+
 const TIMER_STORAGE_KEY = 'vscode-timer:storage-key';
-const MINUTE = 1000; // milli sec in a min
+const MINUTE = 5 * 1000; // milli sec in a min
+
+const CANCEL_TIMER_LABEL = '$(close) Cancel timer';
+const RESET_TIMER_LABEL = '$(clock) Reset timer';
 
 let statusBarItem: vscode.StatusBarItem;
 let timerRef: NodeJS.Timeout;
 
-// this method is called when extension is activated
-// this is activated the very first time the command is executed
+// this method is called the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	vscode.commands.registerCommand('vscode-timer._timer-click', async () => {
-		const option = await vscode.window.showQuickPick(['Cancel timer', 'Reset timer'], { canPickMany: false });
-		if (!option) {
-			return;
-		}
-		if (option === 'Cancel timer') {
-			clearTimeout(timerRef);
-			await _setTimerDurationMins(0, context);
-			updateStatus('0', true);
-			return;
-		}
-		if (option === 'Reset timer') {
-			await vscode.commands.executeCommand('vscode-timer.start');
-			return;
-		}
-	});
+	const addCommand = (id: string, fn: () => void): void => {
+		const cmd = vscode.commands.registerCommand(id, fn);
+		context.subscriptions.push(cmd);
+	};
 
-	let timerStartCmd = vscode.commands.registerCommand('vscode-timer.start', async () => {
+	// status bar temporary message disposable
+	let statusBarDisposableMessage: vscode.Disposable;
+
+	addCommand('vscode-timer.start', async () => {
 		const timerDuration = await vscode.window.showQuickPick(
-			['10m', '15m', '30m', '1h', '2h'],
+			PREDEFINED_TIMER_DURATIONS,
 			{ title: 'Pick a timeframe', canPickMany: false }
 		);
 		if (!timerDuration) {
 			return;
 		}
 
-		await _setTimerDuration(timerDuration, context);
+		// keep track of timer duration
+		await setStateTimerDuration(timerDuration, context);
 
-		// clear previous timers if any and create new timer
-		clearTimeout(timerRef);
-		timerRef = setInterval(async () => {
-			const minsRemaining = _getTimerDurationMins(context) - 1;
-			const cancelTimer = minsRemaining === 0;
-			if (cancelTimer) {
-				clearTimeout(timerRef);
-				updateStatus(minsToTimerDuration(minsRemaining), true);
-				return;
-			}
-			await _setTimerDurationMins(minsRemaining, context);
-			updateStatus(minsToTimerDuration(minsRemaining));
-		}, MINUTE);
+		// clear existing message and show expiration time
+		if (statusBarDisposableMessage) {
+			statusBarDisposableMessage.dispose();
+		}
+		statusBarDisposableMessage = vscode.window.setStatusBarMessage(`Timer expires at ${getExpirationTimestamp(timerDuration)}`, 5000);
 
-		const timestamp = getExpirationTimestamp(timerDuration);
-		vscode.window.setStatusBarMessage(`Timer expires in ${timerDuration} at ${timestamp}`, 3000);
-
+		// create timer status item
 		if (!statusBarItem) {
 			statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
 			statusBarItem.command = 'vscode-timer._timer-click';
 		}
+
+		// clear previous timers if any and create new timer
+		clearTimeout(timerRef);
+		const updateFn = async (firstUpdate: boolean = false) => {
+			const minsRemaining = getStateTimerDurationMins(context) - (firstUpdate ? 0 : 1);
+			const cancelTimer = minsRemaining === 0;
+			if (cancelTimer) {
+				clearTimeout(timerRef);
+				await setStateTimerDurationMins(0, context);
+				updateStatus(minsToTimerDuration(minsRemaining), true);
+				return;
+			}
+			await setStateTimerDurationMins(minsRemaining, context);
+			updateStatus(minsToTimerDuration(minsRemaining));
+		};
+
+		// first update
+		updateFn(true);
+		timerRef = setInterval(updateFn, MINUTE);
 	});
 
-	context.subscriptions.push(timerStartCmd);
+	addCommand('vscode-timer.cancel', async () => {
+		if (getStateTimerDurationMins(context) === 0) {
+			await vscode.window.showWarningMessage('No timer to cancel');
+			return;
+		}
+
+		// clear existing message
+		if (statusBarDisposableMessage) {
+			statusBarDisposableMessage.dispose();
+		}
+
+		clearTimeout(timerRef);
+		await setStateTimerDurationMins(0, context);
+		updateStatus('0', true);
+	});
+
+	// internal command
+	addCommand('vscode-timer._timer-click', async () => {
+		const option = await vscode.window.showQuickPick([CANCEL_TIMER_LABEL, RESET_TIMER_LABEL], { canPickMany: false });
+		if (!option) {
+			return;
+		}
+
+		// clear existing message
+		if (statusBarDisposableMessage) {
+			statusBarDisposableMessage.dispose();
+		}
+
+		if (option === CANCEL_TIMER_LABEL) {
+			await vscode.commands.executeCommand('vscode-timer.cancel');
+		} else if (option === RESET_TIMER_LABEL) {
+			await vscode.commands.executeCommand('vscode-timer.cancel');
+			await vscode.commands.executeCommand('vscode-timer.start');
+		}
+	});
 }
 
-function _getTimerDurationMins(context: vscode.ExtensionContext): number {
+function getStateTimerDurationMins(context: vscode.ExtensionContext): number {
 	return parseInt(context.globalState.get(TIMER_STORAGE_KEY, '0'));
 }
 
-async function _setTimerDuration(timerDuration: string, context: vscode.ExtensionContext): Promise<void> {
-	const [mm, hh] = parseTimerDuration(timerDuration);
-	const mins = mm + hh * 60;
-	await context.globalState.update(TIMER_STORAGE_KEY, mins);
+async function setStateTimerDurationMins(timerDurationMins: number, context: vscode.ExtensionContext): Promise<void> {
+	await context.globalState.update(TIMER_STORAGE_KEY, Math.max(0, timerDurationMins));
 }
 
-async function _setTimerDurationMins(timerDurationMins: number, context: vscode.ExtensionContext): Promise<void> {
-	await context.globalState.update(TIMER_STORAGE_KEY, Math.max(0, timerDurationMins));
+async function setStateTimerDuration(timerDuration: string, context: vscode.ExtensionContext): Promise<void> {
+	const [mm, hh] = parseTimerDuration(timerDuration);
+	const mins = mm + hh * 60;
+	await setStateTimerDurationMins(mins, context);
 }
 
 function updateStatus(timerDuration: string, hide: boolean = false): void {
@@ -104,6 +141,9 @@ function minsToTimerDuration(mins: number): string {
 	const mm = mins % 60;
 	if (hh === 0) {
 		return `${mm}m`;
+	}
+	if (mm === 0) {
+		return `${hh}h`;
 	}
 	return `${hh}h${mm}m`;
 }
